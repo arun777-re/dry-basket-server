b093d403f84b195821d6a122e1bf25d2797cc67c
Arun <arunjamdagni2@gmail.com>
2025-10-23 02:23:28 +0530

    set for forward header

--- src/server.ts
+++ src/server.ts
@@ -45,6 +45,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.set('trust proxy', 1); // trust first proxy
 
 
 
--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,38 +1,44 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
- function createTransporter() {
-        return nodemailer.createTransport({
-            service:"gamil",
-            auth:{
-                type:"OAuth2",
-                user:process.env.SMTP_GMAIL_USER,
-                clientId:process.env.SMTP_CLIENT_ID,
-                clientSecret:process.env.SMTP_CLIENT_SECRET,
-                refreshToken:process.env.SMTP_REFRESH_TOKEN,
-                accessToken:process.env.SMTP_ACCESS_TOKEN,
-            }
-        })
+function createTransporter() {
+  return nodemailer.createTransport({
+    service: "gamil",
+    auth: {
+      type: "OAuth2",
+      user: process.env.SMTP_GMAIL_USER,
+      clientId: process.env.SMTP_CLIENT_ID,
+      clientSecret: process.env.SMTP_CLIENT_SECRET,
+      refreshToken: process.env.SMTP_REFRESH_TOKEN,
+      accessToken: process.env.SMTP_ACCESS_TOKEN,
+    },
+  });
+}
+
+// send email for registration verification
+export async function sendEmailWithNodemailer({
+  to,
+  subject,
+  email,
+}: {
+  to: string;
+  subject: string;
+  email: string;
+}) {
+  try {
+    const transporter = createTransporter();
+    if (!transporter) {
+      throw new Error("No transporter available");
     }
-   
-
-
-            // send email for registration verification
-            export async function sendEmailWithNodemailer({to,subject,email}:{to:string,subject:string,email:string}) {
-                try {
-                    const transporter =  createTransporter();
-                    if(!transporter){
-                        throw new Error("No transporter available");
-                    }
-                   const result =  await transporter.sendMail({
-                        from:`From Dry Basket <${process.env.SMTP_GMAIL_USER}>`,
-                        to,
-                        subject,
-                        html:email,
-                    });
-                    console.log("Email sent successfully:",result.messageId);
-                    return result;
-                } catch (error) {
-                    console.log("Error sending email:",error);
-                    throw new Error("Error sending email");
-                }
-            }
+    const result = await transporter.sendMail({
+      from: `From Dry Basket <${process.env.SMTP_GMAIL_USER}>`,
+      to,
+      subject,
+      html: email,
+    });
+    console.log("Email sent successfully:", result.messageId);
+    return result;
+  } catch (error) {
+    console.log("Error sending email:", error);
+    throw new Error("Error sending email");
+  }
+}
