946b5589d1436e05c9f790d17a26e7b0eb068cfc
Arun <arunjamdagni2@gmail.com>
2025-10-27 01:09:03 +0530

    set upstash url

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,6 +5,7 @@ const REDIS_URL = process.env.UPSTASH_REDIS_REST_URL || 'redis://127.0.0.1:6379'
 export const redisClient = new IORedis(REDIS_URL as string, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
+  tls: {}
 });
 
 redisClient.on('error',(err)=> console.log('Redis Error:',err));
@@ -14,12 +15,14 @@ redisClient.on('connect',()=>console.log('Redis connected'));
 
 // creating a connection using redis with ioredis
 export const bullqConnection= new IORedis(REDIS_URL as string,{
+  tls: {},
     maxRetriesPerRequest:null,
     enableReadyCheck:false
 }); 
 
 // creating a connection using redis with ioredis
 export const bullWorkerConnection= new IORedis(REDIS_URL as string,{
+  tls: {},
     maxRetriesPerRequest:null,
     enableReadyCheck:false
 }); 
