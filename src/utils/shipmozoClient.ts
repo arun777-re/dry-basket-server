656d3aa49f642162c5b0612ac73777425a029b42
Arun <arunjamdagni2@gmail.com>
2025-11-26 01:29:04 +0530

    changes

--- src/utils/shipmozoClient.ts
+++ src/utils/shipmozoClient.ts
@@ -18,7 +18,7 @@ export const shippingRateService = async ({
   weight: number;
 }): Promise<ShippingRateServiceParams> => {
   console.log("shipozoz key:", process.env.SHIPMOZO_PUBLIC_KEY);
-  console.log("pickup pincode:", process.env.PIKUP_PINCODE);
+  console.log("pickup pincode:", process.env.PICKUP_PINCODE);
  const result = await axios.post(
   "https://shipping-api.com/app/api/v1/rate-calculator",
   {
@@ -167,7 +167,7 @@ export const checkPincodeForShipping = async ({
   pickup_pincode: number;
   delivery_pincode: number;
 }): Promise<boolean> => {
- const validPrefixes = ["11", "40", "56", "60", "70", "12", "20", "30", "38"];
+ const validPrefixes = ["11", "40", "56", "60", "70", "12", "20", "30", "38","13","14","15","16","17","18","19","21","22","23","24","25","26","27","28","29","31","32","33","34","35","36","37","39","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","57","58","59","61","62","63","64","65","66","67","68","69","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"];
 
   const isValid =
     String(delivery_pincode).length === 6 &&
